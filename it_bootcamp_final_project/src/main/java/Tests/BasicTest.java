cfd7210f5bb6f3da6e7bac24cb378f9b77a4e64b
Miljana Cvetkovic <miljana.cv@hotmail.com>
2022-06-23 15:35:45 +0200

    Adding a method for screenshot if test fails

--- it_bootcamp_final_project/src/main/java/Tests/BasicTest.java
+++ it_bootcamp_final_project/src/main/java/Tests/BasicTest.java
@@ -1,14 +1,21 @@
 package Tests;
 
 import Pages.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public abstract class BasicTest {
     protected WebDriver driver;
@@ -44,9 +51,21 @@ public abstract class BasicTest {
     }
 
     @AfterMethod
-    public void afterMethod() {
+    public void tearDown(ITestResult result) {
+        String timestamp = new SimpleDateFormat(
+                "hh_mm_ss_dd_MM_yyyy").format(new Date());
 
-    }
+        if (result.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot screen = (TakesScreenshot) driver;
+            File screenshot = screen.getScreenshotAs(OutputType.FILE);
+            try {
+                FileHandler.copy(screenshot, new File(
+                        "src/main/java/Screenshots/TestFailure_" + timestamp + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            }
+        }
 
     @AfterClass
     public void afterClass() {
